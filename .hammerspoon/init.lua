8c04f77bb4a6885fecbe34fed7b09bdebefcd39d
rei-m <star.oceans.rm@gmail.com>
2017-04-09 17:02:42 +0900

    add hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,60 @@
+local function keyCode(key, modifiers)
+   modifiers = modifiers or {}
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()      
+   end
+end
+
+local function remapKey(modifiers, key, keyCode)
+   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
+
+local function enableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:enable()
+   end
+end
+
+remapKey({'ctrl'}, 'h', keyCode('left'))
+remapKey({'ctrl'}, 'j', keyCode('down'))
+remapKey({'ctrl'}, 'k', keyCode('up'))
+remapKey({'ctrl'}, 'l', keyCode('right'))
+remapKey({'ctrl', 'shift'}, 'h', keyCode('left', {'shift'}))
+remapKey({'ctrl', 'shift'}, 'j', keyCode('down', {'shift'}))
+remapKey({'ctrl', 'shift'}, 'k', keyCode('up', {'shift'}))
+remapKey({'ctrl', 'shift'}, 'l', keyCode('right', {'shift'}))
+remapKey({'ctrl', 'cmd'}, 'h', keyCode('left', {'cmd'}))
+remapKey({'ctrl', 'cmd'}, 'j', keyCode('down', {'cmd'}))
+remapKey({'ctrl', 'cmd'}, 'k', keyCode('up', {'cmd'}))
+remapKey({'ctrl', 'cmd'}, 'l', keyCode('right', {'cmd'}))
+remapKey({'ctrl', 'shift', 'cmd'}, 'h', keyCode('left', {'shift', 'cmd'}))
+remapKey({'ctrl', 'shift', 'cmd'}, 'j', keyCode('down', {'shift', 'cmd'}))
+remapKey({'ctrl', 'shift', 'cmd'}, 'k', keyCode('up', {'shift', 'cmd'}))
+remapKey({'ctrl', 'shift', 'cmd'}, 'l', keyCode('right', {'shift', 'cmd'}))
+
+local prevKeyCode
+local escape = 0x35
+local leftCommand = 0x37
+local rightCommand = 0x36
+local eisuu = 0x66
+local kana = 0x68
+
+local function keyStroke(modifiers, character)
+    hs.eventtap.keyStroke(modifiers, character)
+end
+
+local function handleEvent(e)
+    local keyCode = e:getKeyCode()
+    local isCmdKeyUp = not(e:getFlags()['cmd']) and e:getType() == hs.eventtap.event.types.flagsChanged
+    if isCmdKeyUp and prevKeyCode == leftCommand then
+        keyStroke({}, eisuu)
+    elseif isCmdKeyUp and prevKeyCode == rightCommand then
+        keyStroke({}, kana)
+    end
+    prevKeyCode = keyCode
+end
+
+eventtap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged, hs.eventtap.event.types.keyDown, hs.eventtap.event.types.keyUp}, handleEvent)
+eventtap:start()
